77106dd73a03fccfeecda98daac56ddcc4da5268
Nethaiah <115199609+Nethaiah@users.noreply.github.com>
2026-01-22 19:45:18 +0800

    Ayos import

--- src/components/document/manage-pdf-viewer.tsx
+++ src/components/document/manage-pdf-viewer.tsx
@@ -1,7 +1,7 @@
 "use client";
 import dynamic from "next/dynamic";
 
-const PDFViewerClient = dynamic(() => import("../../../features/manage-document/components/pdf-viewer-client"), {
+const PDFViewerClient = dynamic(() => import("@/components/document/manage-pdf-viewer"), {
   ssr: false,
   loading: () => (
     <div className="p-6 text-center text-sm text-gray-600">Loading PDF Viewer…</div>
